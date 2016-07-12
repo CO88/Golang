107bab3a281eacfd16c6746f03047ea5efce19e2
Blanc <Blanc@Blanc-PC>
2016-07-12 18:05:05 +0900

    #23 _1

--- GoExample/src/main/exam_roofAndFunction.go
+++ GoExample/src/main/exam_roofAndFunction.go
@@ -6,11 +6,13 @@ import (
 
 func Sqrt(x float64) float64 {
 	
+	const threshold = 0.0000000000000001
+	
 	z := float64(x/2)
 	tmp := float64(x)
 	// z := 1.0
 	
-	for (tmp - z > 0.0000000000000001) {
+	for (tmp - z > threshold) {
 		tmp = z
 		//NewTon's method 
 		z = z - (z*z -x)/(2*z)
