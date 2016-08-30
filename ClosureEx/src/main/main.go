139ba21c74411916c88c5a6f7e2c26458925ab35
Blanc <Blanc@121212112@naver.com>
2016-08-30 11:52:59 +0900

    stack & Map

--- ClosureEx/src/main/main.go
+++ ClosureEx/src/main/main.go
@@ -5,9 +5,17 @@ import (
 )
 
 func main() {
-	ExampleReadFrom_append()
-	ExampleNewIntGenerator()
+	//ExampleReadFrom_append()
+	//ExampleNewIntGenerator()
 	
-	fmt.Println(Sqrt(25))
+	//fmt.Println(Sqrt(25))
+	
+	fmt.Println(Eval("1 + 2"))
+	fmt.Println(Eval("1 - 2 * 3 + 4"))
+	fmt.Println(Eval("3 * ( 3 + 1 * 3 ) / 2"))
+	
+	hasDupeRune("아아아아")
+	
 }	
+
 
