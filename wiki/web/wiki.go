4297a91655485b2a1244f826dd00e62b88edc820
blankof <121212112@naver.com>
2018-11-26 21:02:02 +0900

    TODO: add frontPage, linking page

---
+++ wiki/web/wiki.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	//"errors"
+)
+
+type Page struct{
+	Title string
+	Body []byte
+}
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var vaildPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title := r.URL.Path[len("/view/"):]
+	
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/" +  title, http.StatusFound)
+		return
+	}
+	
+	//t, _ := template.ParseFiles("view.html")
+	//t.Execute(w, p)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title := r.URL.Path[len("/edit/"):]
+	
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}	
+	//t, _ := template.ParseFiles("edit.html")
+	//t.Execute(w, p)
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title := r.URL.Path[len("/save/"):]
+	
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return 
+	}
+
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+/*
+	change closuer
+	
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("Invalid Page Title")
+    }
+    return m[2], nil // The title is the second subexpression.
+}
+*/
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := vaildPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return 
+		}
+		fn(w, r, m[2])
+	}
+}
+
+
+func main() {
+	//TODO: add /view/frontPage/ and linking page 
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.ListenAndServe(":8080", nil)
+}
+
+
+
