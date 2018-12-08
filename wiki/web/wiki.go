65e4811b3ae8c8c5a2f051a65fa182faac352d29
blankof <121212112@naver.com>
2018-12-08 23:57:24 +0900

    add frontpage and linking in inner page

--- wiki/web/wiki.go
+++ wiki/web/wiki.go
@@ -5,16 +5,20 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"fmt"
 	//"errors"
 )
 
 type Page struct{
 	Title string
 	Body []byte
+	DisplayBody template.HTML
 }
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("edit.html", "view.html","FrontPage.html"))
+//^(start string)...(end string)$
 var vaildPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var pageNameRegexp = regexp.MustCompile("\\[([a-zA-Z0-9]+)\\]")
 var pagePath = "data/"
 
 func (p *Page) save() error {
@@ -25,6 +29,7 @@ func (p *Page) save() error {
 func loadPage(title string) (*Page, error) {
 	filename := pagePath + title + ".txt"
 	body, err := ioutil.ReadFile(filename)
+	
 	if err != nil {
 		return nil, err
 	}
@@ -32,17 +37,28 @@ func loadPage(title string) (*Page, error) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	//title := r.URL.Path[len("/view/"):]
 	
 	p, err := loadPage(title)
+	
+	if title == "FrontPage" {
+		renderTemplate(w, "FrontPage", p)
+		return 
+	}
+	
 	if err != nil {
 		http.Redirect(w, r, "/edit/" +  title, http.StatusFound)
 		return
 	}
+	/*
+		[PageName] -> <a href="/view/PageName">PageName</a>
+	*/
+	escapeBody := []byte(template.HTMLEscapeString(string(p.Body)))
+	p.DisplayBody = template.HTML(pageNameRegexp.ReplaceAllFunc(escapeBody, func(s []byte) []byte {
+			m := string(s[1 : len(s)-1])
+			return []byte("<a href=\"/view/"+ m + "\">" + m +"</a>")
+	}))
 	
-	//t, _ := template.ParseFiles("view.html")
-	//t.Execute(w, p)
-	renderTemplate(w, "view", p)
+	renderTemplate(w, "view", p)//
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -71,9 +87,16 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
     if err != nil {
+    	fmt.Println("error occurred")
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 }
@@ -94,7 +117,8 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	return func(w http.ResponseWriter, r *http.Request) {
 		m := vaildPath.FindStringSubmatch(r.URL.Path)
 		if m == nil {
-			http.NotFound(w, r)
+			//http.NotFound(w, r)
+			http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
 			return 
 		}
 		fn(w, r, m[2])
@@ -107,6 +131,7 @@ func main() {
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/search/", searchHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
